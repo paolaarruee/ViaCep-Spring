e43a6edc70114099e14ecb3597d0b75fb92072aa
Paola Arrueé <paolaarruee@gmail.com>
2021-11-06 02:57:32 -0300

    first commit

---
+++ src/test/java/com/example/ViaCep/ViaCepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ViaCep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaCepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
